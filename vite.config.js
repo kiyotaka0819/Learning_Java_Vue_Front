bb3b618ca1b4f8ffe650c186bfec12615570693a
Kiyotaka <hiroya741@gmail.com>
2026-02-03 01:15:51 +0000

    publicフォルダ作成し、404を移動

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   base: '/Learning_Java_Vue_Front/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        notfound: resolve(__dirname, '404.html')
-      }
-    }
-  }
 })
