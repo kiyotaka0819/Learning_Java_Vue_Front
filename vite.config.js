7b0c2b19e3917a3e8c012fabe80c7a2884cfb0e3
Kiyotaka <hiroya741@gmail.com>
2026-02-03 01:11:46 +0000

    ビルドに404を追加

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/Learning_Java_Vue_Front/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        notfound: resolve(__dirname, '404.html')
+      }
+    }
+  }
 })
